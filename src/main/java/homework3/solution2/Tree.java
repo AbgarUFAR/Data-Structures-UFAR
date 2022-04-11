86473c4e6badba94107a4af5976d6dfa48e373f0
MirAbgarAbrahamyan <abgarabrahamyan1@gmail.com>
2022-04-11 20:32:22 +0400

    got rid of old foreach

--- src/main/java/homework3/solution2/Tree.java
+++ src/main/java/homework3/solution2/Tree.java
@@ -14,7 +14,7 @@ public class Tree {
         private int id;
         private List<Node> children;
 
-        public Node(int data, int parentId, int id) {
+        Node(int data, int parentId, int id) {
             this.data = data;
             this.parentId = parentId;
             this.id = id;
@@ -37,18 +37,18 @@ public class Tree {
     }
 
     public void buildTree(Node parent, JsonArray jsonElements) {
-        for (JsonElement jsonElement : jsonElements) {
-            JsonObject currentNode = jsonElement.getAsJsonObject();
-            if (currentNode.get("parent_id").getAsInt() == parent.id) {
-                parent.children.add(new Node(
-                        currentNode.get("data").getAsInt(),
-                        currentNode.get("parent_id").getAsInt(),
-                        currentNode.get("id").getAsInt()
-                ));
-            }
-        }
-        for (Node child : parent.children) {
-            buildTree(child, jsonElements);
+        jsonElements.forEach(jsonElement -> checkForChildAndAdd(parent, jsonElement));
+        parent.children.forEach(child -> buildTree(child, jsonElements));
+    }
+
+    private void checkForChildAndAdd(Node parent, JsonElement jsonElement) {
+        JsonObject currentNode = jsonElement.getAsJsonObject();
+        if (currentNode.get("parent_id").getAsInt() == parent.id) {
+            parent.children.add(new Node(
+                    currentNode.get("data").getAsInt(),
+                    currentNode.get("parent_id").getAsInt(),
+                    currentNode.get("id").getAsInt()
+            ));
         }
     }
 }
